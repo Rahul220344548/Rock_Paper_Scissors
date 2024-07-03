72facc1fdf7dafc6844ea9f59cac1f0db98afc31
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-03 19:06:19 +0100

    create a simple algorithm to generate random computer choice

--- js/javascript.js
+++ js/javascript.js
@@ -6,11 +6,21 @@ function getComputerChoice() {
     let choice2 = "paper";
     let choice3 = "scissors";
 
-    console.log(Math.random());
+    
 
     let prob = Math.random();
 
     
 
+    if (prob > 0 && prob < 0.333) {
+        return console.log(choice1);
+    }
+    else if (prob > 0.333 && prob < 0.666) {
+        return console.log(choice2);
+    } else {
+        return console.log(choice3);;
+    }
+
 }
 
+getComputerChoice();
